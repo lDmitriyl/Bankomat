62516c45bdb6bd5c38d09eee90556b0ce2f26c3f
lDmitriyl <37330858+lDmitriyl@users.noreply.github.com>
2021-04-28 09:35:10 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
             <form>
                 <label for="code">Номинал в наличии: </label><br>
-                <input type="text" name="banknotes" placeholder="5, 10, 20, 50, 100, 200, 500 "><br>
+                <input type="text" name="banknotes"><br>
                 <label for="code">Ваша сумма: </label><br>
                 <input type="text" name="sum" placeholder=""></br>
                 <div class="form-btn">
